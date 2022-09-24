eaa0d45b8e6212e0dd343baa7818b6bc1d531e03
Mert Can Cetinok <69702906+mertcancetinok@users.noreply.github.com>
2022-09-25 02:12:58 +0300

    header add

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import logo from './logo.png';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__left">
+            <img src={logo} alt="" />
+            <div className="header__search">
+                <SearchIcon/>
+                <input type="text" />
+            </div>
+        </div>
+
+        <div className="header__right">
+            <HeaderOption Icon={HomeIcon} title='Home'/>
+            <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+            <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+            <HeaderOption Icon={ChatIcon} title='Messaging'/>
+            <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+            <HeaderOption avatar='https://mui.com/static/images/avatar/1.jpg' title='me'/>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
