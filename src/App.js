1757c61d9683e0a3844e03102c9fc5cd30f9e630
Mert Can Cetinok <69702906+mertcancetinok@users.noreply.github.com>
2022-10-08 13:33:22 +0300

    sidebar and feed > input add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
-import Header from "./Header";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div className="app__body">
+                <Sidebar/>
+                <Feed/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
